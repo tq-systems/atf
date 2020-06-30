b5c4078a87f290a0cd7382d08fbcc04db3855710
Matthias Schiffer <matthias.schiffer@tq-group.com>
2020-06-30 15:03:11 +0200

    nxp: soc-ls1012: add tqmls1012al board
    RAM timings are ported from U-Boot. Only the 512MB variant is supported,
    as no other variants will be released for now.
    
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

---
+++ plat/nxp/soc-ls1012/tqmls1012al/ddr_init.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Author York Sun <york.sun@nxp.com>
+ */
+
+#include <platform_def.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <debug.h>
+#include <errno.h>
+#include <string.h>
+#include <io.h>
+#include <fsl_mmdc.h>
+
+long long _init_ddr(void)
+{
+	/* 512MB variant */
+	static const struct fsl_mmdc_info mparam = {
+		.mdctl = 0x04180000,
+		.mdpdc = 0x00030036,
+		.mdotc = 0x12444040,
+		.mdcfg0 = 0x82877994,
+		.mdcfg1 = 0xDB328F84,
+		.mdcfg2 = 0x01FF0124,
+		.mdmisc = 0x00200680,
+		.mdref = 0x079E8000,
+		.mdrwd = 0x00002000,
+		.mdor = 0x00871023,
+		.mdasp = 0x0000003f,
+		.mpodtctrl = 0x0000022A,
+		.mpzqhwctrl = 0xA1390003,
+	};
+
+	mmdc_init(&mparam);
+	NOTICE("DDR Init Done\n");
+
+	/* Need to see there is any other way to
+	 * deduce the total DRAM size rather
+	 * hard-coding it.
+	 */
+	return NXP_DRAM0_SIZE;
+}
