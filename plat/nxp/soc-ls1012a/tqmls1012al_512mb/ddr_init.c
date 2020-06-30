4798bafca9c88bff2403ecfb68767f8b87b54213
Matthias Schiffer <matthias.schiffer@tq-group.com>
2020-06-30 15:03:11 +0200

    nxp: soc-ls1012: add tqmls1012al board with 512MiB RAM
    RAM timings are ported from U-Boot.
    
    Change-Id: Idc3656f26338212dc6970a9c450c1946e357b556
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>
    Signed-off-by: Alexander Stein <alexander.stein@tq-group.com>

---
+++ plat/nxp/soc-ls1012a/tqmls1012al_512mb/ddr_init.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018-2022 NXP
+ * Copyright 2022-2025 TQ-Systems GmbH
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <fsl_mmdc.h>
+
+#include "platform_def.h"
+
+long long init_ddr(void)
+{
+	/* 512MiB variant */
+	static const struct fsl_mmdc_info mparam = {
+		.mdctl = U(0x04180000),
+		.mdpdc = U(0x00030036),
+		.mdotc = U(0x12444040),
+		.mdcfg0 = U(0x82877994),
+		.mdcfg1 = U(0xDB328F84),
+		.mdcfg2 = U(0x01FF0124),
+		.mdmisc = U(0x00200680),
+		.mdref = U(0x079E8000),
+		.mdrwd = U(0x00002000),
+		.mdor = 0x00871023,
+		.mdasp = U(0x0000003f),
+		.mpodtctrl = U(0x0000022A),
+		.mpzqhwctrl = U(0xA1390003),
+	};
+
+	mmdc_init(&mparam, NXP_DDR_ADDR);
+	NOTICE("DDR 512 MiB Init Done\n");
+
+	return NXP_DRAM0_SIZE;
+}
