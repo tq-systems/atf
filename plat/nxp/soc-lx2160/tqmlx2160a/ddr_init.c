3928092e12f98b958b53ab1330c2283ebd9e78d7
Gregor Herburger <gregor.herburger@tq-group.com>
2021-02-25 17:14:50 +0100

    tqmlx2160a: change ddr parameters to enable 2800 MT/s
    Also remove switch function as only one ddr ram configuration on the
    board is supported.
    
    Basically change the following:
    Change rtt_park from 240 to 60, odt from 48 to 60 and add
    output_driver_impedance with 1.
    
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

--- plat/nxp/soc-lx2160/tqmlx2160a/ddr_init.c
+++ plat/nxp/soc-lx2160/tqmlx2160a/ddr_init.c
@@ -88,43 +88,26 @@ int ddr_get_ddr_params(struct dimm_params *pdimm,
 int ddr_board_options(struct ddr_info *priv)
 {
 	struct memctl_opt *popts = &priv->opt;
-	const struct ddr_conf *conf = &priv->conf;
 
 	popts->vref_dimm = 0x24;		/* range 1, 83.4% */
 	popts->rtt_override = 0;
-	popts->rtt_park = 240;
+	popts->rtt_park = 60;
 	popts->otf_burst_chop_en = 0;
 	popts->burst_length = DDR_BL8;
 	popts->trwt_override = 1;
 	popts->bstopre = 0;			/* auto precharge */
 	popts->addr_hash = 1;
 
-#if DDRC_NUM_DIMM != 2
-#error This board has two DIMM slots per controller.
-#endif
-	/* Set ODT impedance on PHY side */
-	switch (conf->cs_on_dimm[1]) {
-	case 0xc:	/* Two slots dual rank */
-	case 0x4:	/* Two slots single rank, not valid for interleaving */
-		popts->trwt = 0xf;
-		popts->twrt = 0x7;
-		popts->trrt = 0x7;
-		popts->twwt = 0x7;
-		popts->vref_phy = 0x6B;	/* 83.6% */
-		popts->odt = 60;
-		popts->phy_tx_impedance = 28;
-		break;
-	case 0:		/* Ont slot used */
-	default:
-		popts->trwt = 0x3;
-		popts->twrt = 0x3;
-		popts->trrt = 0x3;
-		popts->twwt = 0x3;
-		popts->vref_phy = 0x60;	/* 75% */
-		popts->odt = 48;
-		popts->phy_tx_impedance = 48;
-		break;
-	}
+	popts->trwt = 0x3;
+	popts->twrt = 0x3;
+	popts->trrt = 0x3;
+	popts->twwt = 0x3;
+	popts->vref_phy = 0x60;	/* 75% */
+	popts->odt = 60;
+	popts->phy_tx_impedance = 48;
+	popts->output_driver_impedance = 1;
+	debug("TQMLX2160a board ddr4 settings: RTT_park: %u - ODT: %u - output driver impedance: %d\n",
+	      popts->rtt_park, popts->odt, popts->output_driver_impedance);
 
 	return 0;
 }
