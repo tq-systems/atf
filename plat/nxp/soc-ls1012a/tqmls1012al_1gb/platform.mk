a857a81ce8b1f8a2a4fbd7f23f2dec4746a7fa33
Alexander Stein <alexander.stein@tq-group.com>
2022-05-03 11:09:24 +0200

    nxp: soc-ls1012: add tqmls1012al board with 1GB RAM
    Add a variant with 1GB RAM timings.
    
    Signed-off-by: Max Merchel <Max.Merchel@tq-group.com>
    Signed-off-by: Alexander Stein <alexander.stein@tq-group.com>

---
+++ plat/nxp/soc-ls1012a/tqmls1012al_1gb/platform.mk
@@ -0,0 +1,26 @@
+#
+# Copyright 2018 NXP
+# Copyright 2022 TQ-Systems GmbH
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# board-specific build parameters
+BOOT_MODE	:= qspi
+BOARD		:= tqmls1012al_1gb
+
+# DDR Compilation Configs
+DDRC_NUM_CS     :=      1
+
+# On-Board Flash Details
+QSPI_FLASH_SZ   :=      0x4000000
+
+BL2_SOURCES	+=	${BOARD_PATH}/ddr_init.c
+
+SUPPORTED_BOOT_MODE	:=	qspi
+
+# Adding platform board build info
+include plat/nxp/common/plat_common_def.mk
+
+# Adding SoC build info
+include plat/nxp/soc-ls1012a/soc.mk
