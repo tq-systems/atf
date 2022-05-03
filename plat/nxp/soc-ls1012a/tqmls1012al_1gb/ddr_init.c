5bfa337de7072a5debc1f3ed80c5b51155881ef6
Alexander Stein <alexander.stein@tq-group.com>
2022-05-03 11:09:24 +0200

    nxp: soc-ls1012: add tqmls1012al board with 1GB RAM
    Add a variant with 1GB RAM timings.
    
    Signed-off-by: Max Merchel <Max.Merchel@tq-group.com>
    Signed-off-by: Alexander Stein <alexander.stein@tq-group.com>

--- plat/nxp/soc-ls1012a/tqmls1012al_1gb/ddr_init.c
+++ plat/nxp/soc-ls1012a/tqmls1012al_1gb/ddr_init.c
@@ -18,25 +18,25 @@
 
 long long init_ddr(void)
 {
-	/* 512MB variant */
+	/* 1GB variant */
 	static const struct fsl_mmdc_info mparam = {
-		.mdctl = 0x04180000,
+		.mdctl = 0x05180000,
 		.mdpdc = 0x00030036,
 		.mdotc = 0x12444040,
-		.mdcfg0 = 0x82877994,
+		.mdcfg0 = 0xAAB47994,
 		.mdcfg1 = 0xDB328F84,
 		.mdcfg2 = 0x01FF0124,
 		.mdmisc = 0x00200680,
 		.mdref = 0x079E8000,
 		.mdrwd = 0x00002000,
-		.mdor = 0x00871023,
+		.mdor = 0x00B41023,
 		.mdasp = 0x0000003f,
 		.mpodtctrl = 0x0000022A,
 		.mpzqhwctrl = 0xA1390003,
 	};
 
 	mmdc_init(&mparam, NXP_DDR_ADDR);
-	NOTICE("DDR 512 MB Init Done\n");
+	NOTICE("DDR 1 GB Init Done\n");
 
 	return NXP_DRAM0_SIZE;
 }
---
+++ plat/nxp/soc-ls1012a/tqmls1012al_1gb/platform_def.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2018-2019, 2021 NXP
+ * Copyright 2022 TQ-Systems GmbH
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLATFORM_DEF_H
+#define PLATFORM_DEF_H
+
+#include <plat_def.h>
+#include <plat_default_def.h>
+
+#endif /* PLATFORM_DEF_H */
