f75ceb3a0ab54e6f72f839ac28dab6fd7be9fec9
Gregor Herburger <gregor.herburger@tq-group.com>
2022-06-21 08:37:22 +0200

    plat: nxp: soc-ls1028a: Add tqmls1028a_1gb variant
    Change-Id: If16a09543aa820d2e7e89a2fb005988dc8ee1ee2
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

---
+++ plat/nxp/soc-ls1028a/tqmls1028a_1gb/platform.mk
@@ -0,0 +1,34 @@
+#
+# Copyright 2018-2021 NXP
+# Copyright 2022-2025 TQ-Systems GmbH
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# Board-specific build parameters
+BOOT_MODE	?=	flexspi_nor
+BOARD		:=	tqmls1028a_1gb
+POVDD_ENABLE	:=	no
+WARM_BOOT	:=	no
+
+# DDR build parameters
+NUM_OF_DDRC		:=	1
+DDRC_NUM_DIMM		:=	1
+CONFIG_STATIC_DDR	:=	1
+
+FLASH_TYPE	:=	MX25U51245G
+XSPI_FLASH_SZ	:=	0x4000000
+
+BL2_SOURCES	+=	${BOARD_PATH}/ddr_init.c \
+
+SUPPORTED_BOOT_MODE	:=	flexspi_nor	\
+				sd		\
+				emmc		\
+				mmcsd		\
+				auto
+
+# Add platform board build info
+include plat/nxp/common/plat_make_helper/plat_common_def.mk
+
+# Add SoC build info
+include plat/nxp/soc-ls1028a/soc.mk
