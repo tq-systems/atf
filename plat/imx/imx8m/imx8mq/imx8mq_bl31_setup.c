8715820ab7028541ab60e71f49009f9c850c1a76
Lucas Stach <l.stach@pengutronix.de>
2022-12-08 16:00:04 +0100

    feat(imx8mq): always set up console
    This aligns the i.MX8MQ platform behaviour with the other i.MX8M*
    platforms by always setting up the console UART.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Change-Id: I1279d9cb4feb6e789422b9844cab711b8daae74e
    (cherry picked from commit 36be10861e851e7e4df06bb08aab60d8e878d2b2)
    
    Resolved merge conflicts in vendor code.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- plat/imx/imx8m/imx8mq/imx8mq_bl31_setup.c
+++ plat/imx/imx8m/imx8mq/imx8mq_bl31_setup.c
@@ -145,6 +145,7 @@ static void bl31_tzc380_setup(void)
 void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 			u_register_t arg2, u_register_t arg3)
 {
+	static console_t console;
 	int i;
 	/* enable CSU NS access permission */
 	for (i = 0; i < 64; i++) {
@@ -155,12 +156,11 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 
 	imx8m_caam_init();
 
-#if DEBUG_CONSOLE
-	static console_t console;
-
 	console_imx_uart_register(IMX_BOOT_UART_BASE, IMX_BOOT_UART_CLK_IN_HZ,
 		IMX_CONSOLE_BAUDRATE, &console);
-#endif
+	/* This console is only used for boot stage */
+	console_set_scope(&console, CONSOLE_FLAG_BOOT);
+
 	/*
 	 * tell BL3-1 where the non-secure software image is located
 	 * and the entry state information.
--- plat/imx/imx8m/imx8mq/include/platform_def.h
+++ plat/imx/imx8m/imx8mq/include/platform_def.h
@@ -159,13 +159,4 @@
 #define CNTFID1_OFF			U(0x24)
 #define COUNTER_FREQUENCY		8000000 /* 8MHz */
 
-#define DEBUG_CONSOLE			0
-
-#ifdef IMX_ANDROID_BUILD
-#if !DEBUG_CONSOLE
-#undef LOG_LEVEL
-#define LOG_LEVEL 			0
-#endif
-#endif
-
 #define IMX_WDOG_B_RESET
