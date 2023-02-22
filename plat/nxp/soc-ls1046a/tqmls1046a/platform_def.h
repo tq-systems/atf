63cf4b83ac5ccddd4aa467f2dbf739d3c1f56256
Gregor Herburger <gregor.herburger@tq-group.com>
2023-02-22 13:30:10 +0000

    tqmls1046a: add initial support for the 2 GiB variant
    RAM timings are ported from U-Boot (TQMLS10xxA-u-boot-v2018.07)
    
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

---
+++ plat/nxp/soc-ls1046a/tqmls1046a/platform_def.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2018-2021 NXP
+ * Copyright (c) 2023 TQ-Systems GmbH <oss@tq-group.com>, D-82229 Seefeld, Germany.
+ * Author: Gregor Herburger
+ *
+ */
+
+#ifndef PLATFORM_DEF_H
+#define PLATFORM_DEF_H
+
+#include <plat_def.h>
+#include <plat_default_def.h>
+
+#endif /* PLATFORM_DEF_H */
