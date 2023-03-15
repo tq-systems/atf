bdb44b2e2f5c48af451cc72e21697ddf60e9494e
Gregor Herburger <gregor.herburger@tq-group.com>
2023-03-15 16:05:20 +0000

    plat: nxp: soc-ls1088a: Add tqmls1088a_2gb variant
    RAM timings are ported from U-Boot (TQMLS10xxA-u-boot-v2018.07)
    
    Change-Id: Ic6c42d80542f5f24da425d31125072c792a4392a
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

---
+++ plat/nxp/soc-ls1088a/tqmls1088a_2gb/platform_def.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2018-2022 NXP
+ * Copyright (c) 2023-2025 TQ-Systems GmbH <oss@ew.tq-group.com>, D-82229 Seefeld, Germany.
+ * Author: Gregor Herburger
+ *
+ */
+
+#ifndef PLATFORM_DEF_H
+#define PLATFORM_DEF_H
+
+#include <plat_def.h>
+#include <plat_default_def.h>
+
+#endif /* PLATFORM_DEF_H */
