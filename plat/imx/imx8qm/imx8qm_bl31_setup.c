varigit/imx-atf

commit 50b4e5f3c5e97fe8a9690b7e36d377a63df71435

imx: fix partition reboot fail when debug console is enabled
With partition reboot enabled, console_list variable which is
located in data section is NOT reset, system will be busy looping
in early console operation of flush_loop() if console_list is
NOT 0 while HW console is NOT initialized, so we have to clear
this variable to make partition reboot work.

Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

plat/imx/imx8qm/imx8qm_bl31_setup.c
@@ -37,6 +37,10 @@ static const unsigned long BL31_RW_END			= BL_END;
 
 IMPORT_SYM(unsigned long, __RW_START__, BL31_RW_START);
 
+#if DEBUG_CONSOLE
+extern unsigned long console_list;
+#endif
+
 static entry_point_info_t bl32_image_ep_info;
 static entry_point_info_t bl33_image_ep_info;
 
@@ -314,6 +318,8 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 {
 #if DEBUG_CONSOLE
 	static console_t console;
+
+	console_list = 0;
 #endif
 	if (sc_ipc_open(&ipc_handle, SC_IPC_BASE) != SC_ERR_NONE)
 		panic();

plat/imx/imx8qx/imx8qx_bl31_setup.c
@@ -37,6 +37,10 @@ static const unsigned long BL31_RW_END			= BL_END;
 
 IMPORT_SYM(unsigned long, __RW_START__, BL31_RW_START);
 
+#if DEBUG_CONSOLE
+extern unsigned long console_list;
+#endif
+
 static entry_point_info_t bl32_image_ep_info;
 static entry_point_info_t bl33_image_ep_info;
 
@@ -320,6 +324,8 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 {
 #if DEBUG_CONSOLE
 	static console_t console;
+
+	console_list = 0;
 #endif
 	if (sc_ipc_open(&ipc_handle, SC_IPC_BASE) != SC_ERR_NONE)
 		panic();

tq-systems/atf

commit 2e90163aa6538f0b8fd02d47ba3a6aece3e119ff

fix(imx8qm): wrong extern declaration for console_list
The console_list variable is indirect provided by debug.h. Remove
declaration brought in long time ago.

Fixes: 50b4e5f3c5e9 ("imx: fix partition reboot fail when debug console is enabled")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

plat/imx/imx8qm/imx8qm_bl31_setup.c
@@ -41,10 +41,6 @@ IMPORT_SYM(unsigned long, __RW_START__, BL31_RW_START);
 IMPORT_SYM(unsigned long, __DATA_START__, BL31_DATA_START);
 IMPORT_SYM(unsigned long, __DATA_END__, BL31_DATA_END);
 
-#if DEBUG_CONSOLE
-extern unsigned long console_list;
-#endif
-
 static entry_point_info_t bl32_image_ep_info;
 static entry_point_info_t bl33_image_ep_info;
 