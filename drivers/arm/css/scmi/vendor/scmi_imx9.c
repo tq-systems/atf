natashaKoval/imx-atf

commit 5e2ae77510038eccbab3ec7b453481ca74b2c8e4

LF-13235 feat(imx95): support LM boot and shutdown
Add support for LM boot and shutdown operations on i.MX95.

Change-Id: I91c43d92cb53cf26080150a0b261e2efe6351f8e
Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

drivers/arm/css/scmi/vendor/scmi_imx9.c
@@ -13,6 +13,66 @@
 #include "../scmi_private.h"
 #include "scmi_imx9.h"
 
+int scmi_lmm_shutdown(void *p, uint32_t lm_id, uint32_t flags)
+{
+	mailbox_mem_t *mbx_mem;
+	int ret;
+	scmi_channel_t *ch = (scmi_channel_t *)p;
+
+	validate_scmi_channel(ch);
+
+	scmi_get_channel(ch);
+
+	mbx_mem = (mailbox_mem_t *)(ch->info->scmi_mbx_mem);
+	mbx_mem->msg_header = SCMI_MSG_CREATE(IMX9_SCMI_LMM_PROTO_ID,
+					      IMX9_SCMI_LMM_SHUTDOWN_MSG, 0x0);
+	mbx_mem->len = IMX9_SCMI_LMM_SHUTDOWN_MSG_LEN;
+	mbx_mem->flags = SCMI_FLAG_RESP_POLL;
+
+	SCMI_PAYLOAD_ARG2(mbx_mem->payload, lm_id, flags);
+
+	scmi_send_sync_command(ch);
+
+	/* get return values */
+	SCMI_PAYLOAD_RET_VAL1(mbx_mem->payload, ret);
+	assert(mbx_mem->len == IMX9_SCMI_LMM_SHUTDOWN_RESP_LEN);
+	assert(0x0 == SCMI_MSG_GET_TOKEN(mbx_mem->msg_header));
+
+	scmi_put_channel(ch);
+
+	return ret;
+}
+
+int scmi_lmm_boot(void *p, uint32_t lm_id)
+{
+	mailbox_mem_t *mbx_mem;
+	int ret;
+	scmi_channel_t *ch = (scmi_channel_t *)p;
+
+	validate_scmi_channel(ch);
+
+	scmi_get_channel(ch);
+
+	mbx_mem = (mailbox_mem_t *)(ch->info->scmi_mbx_mem);
+	mbx_mem->msg_header = SCMI_MSG_CREATE(IMX9_SCMI_LMM_PROTO_ID,
+					      IMX9_SCMI_LMM_BOOT_MSG, 0x0);
+	mbx_mem->len = IMX9_SCMI_LMM_BOOT_MSG_LEN;
+	mbx_mem->flags = SCMI_FLAG_RESP_POLL;
+
+	SCMI_PAYLOAD_ARG1(mbx_mem->payload, lm_id);
+
+	scmi_send_sync_command(ch);
+
+	/* get return values */
+	SCMI_PAYLOAD_RET_VAL1(mbx_mem->payload, ret);
+	assert(mbx_mem->len == IMX9_SCMI_LMM_BOOT_RESP_LEN);
+	assert(token == SCMI_MSG_GET_TOKEN(mbx_mem->msg_header));
+
+	scmi_put_channel(ch);
+
+	return ret;
+}
+
 /*
  * API to set the SCMI AP core reset address and attributes
  */

drivers/arm/css/scmi/vendor/scmi_imx9.h
@@ -92,6 +92,19 @@
 #define SCMI_CPU_VEC_FLAGS_BOOT			BIT(30)
 #define SCMI_CPU_VEC_FLAGS_RESUME		BIT(31)
 
+#define IMX9_SCMI_LMM_PROTO_ID			0x80
+
+#define IMX9_SCMI_LMM_BOOT_MSG                  0x4
+#define IMX9_SCMI_LMM_BOOT_MSG_LEN              0x8
+#define IMX9_SCMI_LMM_BOOT_RESP_LEN             0x8
+
+#define IMX9_SCMI_LMM_SHUTDOWN_MSG		0x6
+#define IMX9_SCMI_LMM_SHUTDOWN_MSG_LEN		0xc
+#define IMX9_SCMI_LMM_SHUTDOWN_RESP_LEN		0x8
+
+#define IMX9_SCMI_LMM_SHUTDOWN_FLAG_FORCE	BIT(0)
+#define IMX9_SCMI_LMM_SHUTDOWN_FLAG_GRACEFUL	BIT(1)
+
 #define SCMI_GPC_WAKEUP 			0
 #define SCMI_GIC_WAKEUP				1
 #define SCMI_RESUME_CPU				BIT(1)
@@ -160,5 +173,7 @@ int scmi_core_lpm_mode_set(void *p, uint32_t cpu_id, uint32_t num_configs,
 int scmi_per_lpm_mode_set(void *p, uint32_t cpu_id, uint32_t num_configs,
 			   struct scmi_per_lpm_config *cfg);
 int scmi_perf_mode_set(void *p, uint32_t domain_id, uint32_t perf_level);
+int scmi_lmm_boot(void *p, uint32_t lm_id);
+int scmi_lmm_shutdown(void *p, uint32_t lm_id, uint32_t flags);
 
 #endif /* SCMI_IMX9_H */

plat/imx/common/imx_sip_svc.c
@@ -106,6 +106,11 @@ static uintptr_t imx_sip_handler(unsigned int smc_fid,
 		SMC_RET1(handle, imx_src_handler(smc_fid, x1, x2, x3, handle));
 		break;
 #endif
+#if defined(PLAT_imx95)
+	case IMX_SIP_LMM:
+		SMC_RET1(handle, imx_lmm_handler(smc_fid, x1, x2, x3, handle));
+		break;
+#endif
 #if defined(PLAT_imx8qm) && defined(SPD_trusty)
 	case IMX_SIP_CONFIGURE_MEM_FOR_VPU:
 		return imx_configure_memory_for_vpu(handle, x1);

plat/imx/common/include/imx_sip_svc.h
@@ -77,6 +77,10 @@ int imx_kernel_entry_handler(uint32_t smc_fid, u_register_t x1,
 
 #define IMX_SIP_HIFI_XRDC               0xC200000E
 
+#define IMX_SIP_LMM			0xC200000F
+#define IMX_SIP_LMM_BOOT		0x0
+#define IMX_SIP_LMM_SHUTDOWN		0x1
+
 #if defined(PLAT_imx8qm) && defined(SPD_trusty)
 #define IMX_SIP_CONFIGURE_MEM_FOR_VPU       0xC2000010
 #define IMX_SIP_GET_PARTITION_NUMBER        0xC2000011
@@ -148,6 +152,8 @@ int dram_dvfs_handler(uint32_t smc_fid, void *handle,
 #endif
 #if defined(PLAT_imx95)
 int imx_src_handler(uint32_t smc_fid, u_register_t x1,
+		    u_register_t x2, u_register_t x3, void *handle);
+int imx_lmm_handler(uint32_t smc_fid, u_register_t x1,
 		    u_register_t x2, u_register_t x3, void *handle);
 #endif
 #endif /* __IMX_SIP_SVC_H__ */

plat/imx/imx95/imx95_lmm.c
@@ -1,0 +1,30 @@
+#include <drivers/arm/css/scmi.h>
+#include <imx_sip_svc.h>
+#include <scmi_imx9.h>
+#include <common/runtime_svc.h>
+
+extern void *imx95_scmi_handle;
+
+int imx_lmm_handler(uint32_t smc_fid, u_register_t x1, u_register_t x2,
+		    u_register_t x3, void *handle)
+{
+	int ret;
+
+	switch (x1) {
+	case IMX_SIP_LMM_BOOT:
+		ret = scmi_lmm_boot(imx95_scmi_handle, x2);
+		if (ret)
+			return ret;
+		break;
+	case IMX_SIP_LMM_SHUTDOWN:
+		ret = scmi_lmm_shutdown(imx95_scmi_handle, x2,
+					IMX9_SCMI_LMM_SHUTDOWN_FLAG_GRACEFUL);
+		if (ret)
+			return ret;
+		break;
+	default:
+		return SMC_UNK;
+	}
+
+	return 0;
+}

plat/imx/imx95/platform.mk
@@ -37,6 +37,7 @@ BL31_SOURCES		+=	drivers/arm/css/scmi/scmi_common.c		\
 				plat/imx/common/lpuart_console.S		\
 				plat/imx/imx95/imx95_bl31_setup.c		\
 				plat/imx/imx95/imx95_m7.c			\
+				plat/imx/imx95/imx95_lmm.c			\
 				lib/cpus/aarch64/cortex_a55.S			\
 				drivers/delay_timer/delay_timer.c		\
 				drivers/delay_timer/generic_delay_timer.c	\

tq-systems/atf

commit a1946103fbb4718e9e41b947542b270c9530818b

feat(imx95): fix LM boot compiling with DEBUG
Fix missing variable for assert statement.

Fixes: 5e2ae7751003 ("LF-13235 feat(imx95): support LM boot and shutdown")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

drivers/arm/css/scmi/vendor/scmi_imx9.c
@@ -66,7 +66,7 @@ int scmi_lmm_boot(void *p, uint32_t lm_id)
 	/* get return values */
 	SCMI_PAYLOAD_RET_VAL1(mbx_mem->payload, ret);
 	assert(mbx_mem->len == IMX9_SCMI_LMM_BOOT_RESP_LEN);
-	assert(token == SCMI_MSG_GET_TOKEN(mbx_mem->msg_header));
+	assert(0x0 == SCMI_MSG_GET_TOKEN(mbx_mem->msg_header));
 
 	scmi_put_channel(ch);
 