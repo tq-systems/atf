2ae443508f0aa7183c7b7365c03740dd435e3c09
Alexander Stein <alexander.stein@ew.tq-group.com>
2025-03-07 13:06:33 +0100

    fix(imx8m): add RDC entries for all missing UARTs
    Commit f7434fa13 ("fix(imx8m): ensure domain permissions for the
    console") sets RDC domain to Cortex-A only for console UART. But
    this only works if there is an RDC configuration entry provided.
    Add missing RDC entries using the (reset) default value, so
    imx_rdc_console_access_enable() can actually configuration domain
    access.
    
    Fixes: f7434fa13507 ("fix(imx8m): ensure domain permissions for the console")
    Change-Id: I550dbf2b53795ab43839139c455f2468442a3251
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>
    (cherry picked from commit a2c6e11dbc8e9991e0cf44be713140fec915f470)
    
    Conflicts:
    	plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
    	plat/imx/imx8m/imx8mn/imx8mn_bl31_setup.c
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- plat/imx/imx8m/imx8mn/imx8mn_bl31_setup.c
+++ plat/imx/imx8m/imx8mn/imx8mn_bl31_setup.c
@@ -62,8 +62,10 @@ static struct imx_rdc_cfg rdc[] = {
 	RDC_MDAn(RDC_MDA_M7, DID1),
 
 	/* peripherals domain permission */
-	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_UART1, D0R | D0W | D1R | D1W | D2R | D2W | D3R | D3W),
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_UART3, D0R | D0W | D1R | D1W | D2R | D2W | D3R | D3W),
+	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
 	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R),
 
 	/* memory region */
@@ -110,9 +112,10 @@ static struct imx_rdc_cfg rdc[] = {
 	RDC_MDAn(RDC_MDA_M7, DID1),
 
 	/* peripherals domain permission */
-	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_UART1, D0R | D0W | D1R | D1W | D2R | D2W | D3R | D3W),
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
-	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R),
+	RDC_PDAPn(RDC_PDAP_UART3, D0R | D0W | D1R | D1W | D2R | D2W | D3R | D3W),
+	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
 
 	/* memory region */
 	RDC_MEM_REGIONn(16, 0x0, 0x0, 0xff),
