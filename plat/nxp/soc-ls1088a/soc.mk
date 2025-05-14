jolivain/trusted-firmware-a

fix(layerscape): unlock write access SMMU_CBn_ACTLR
This patch is to fix Errata #841119 and #826419 failed apply in linux
because of SMMU_CBn_ACTLR register can't be modified in non-secure
states.

Signed-off-by: Howard Lu <howard.lu@nxp.com>
Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>
Change-Id: I2b23e7c8baa809f385917eb45b10ec6b26a9ada8

plat/nxp/soc-ls1088a/soc.mk
@@ -23,12 +23,12 @@ include ${PLAT_COMMON_PATH}/plat_make_helper/plat_build_macros.mk
 
 # For Security Features
 DISABLE_FUSE_WRITE	:= 1
+$(eval $(call SET_NXP_MAKE_FLAG,SMMU_NEEDED,BL2))
 ifeq (${TRUSTED_BOARD_BOOT}, 1)
 ifeq (${GENERATE_COT},1)
 # Save Keys to be used by DDR FIP image
 SAVE_KEYS=1
 endif
-$(eval $(call SET_NXP_MAKE_FLAG,SMMU_NEEDED,BL2))
 $(eval $(call SET_NXP_MAKE_FLAG,SFP_NEEDED,BL2))
 $(eval $(call SET_NXP_MAKE_FLAG,SNVS_NEEDED,BL2))
 # Used by create_pbl tool to

tq-systems/atf

(cherry picked from commit 0ca1d8fba3bee32242b123ae28ad5c83a657aa0d)
Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

plat/nxp/soc-ls1088a/soc.mk
@@ -17,12 +17,12 @@ SEPARATE_RW_AND_NOLOAD	:= 1
 
 # For Security Features
 DISABLE_FUSE_WRITE	:= 1
+$(eval $(call SET_FLAG,SMMU_NEEDED,BL2))
 ifeq (${TRUSTED_BOARD_BOOT}, 1)
 ifeq (${GENERATE_COT},1)
 # Save Keys to be used by DDR FIP image
 SAVE_KEYS=1
 endif
-$(eval $(call SET_FLAG,SMMU_NEEDED,BL2))
 $(eval $(call SET_FLAG,SFP_NEEDED,BL2))
 $(eval $(call SET_FLAG,SNVS_NEEDED,BL2))
 # Used by create_pbl tool to